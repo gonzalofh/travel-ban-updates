dcaac2560eaf76898121a24d4394bf0e30c83cb1
gonzalofh <gonzalofh@users.noreply.github.com>
2021-08-07 19:52:34 -0500

    fixed regex

--- src/dateScraper.js
+++ src/dateScraper.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const selector = 'div.contentbody';
-const dateRe = new RegExp('Last updated: (\w{1,9} \d{1,2}, 2021)');
+const dateRe = new RegExp('Last updated: (\\w{1,9} \\d{2}, 2021)');
 
 async function loadPage(browser, url) {
     const page = await browser.newPage();
@@ -25,7 +25,12 @@ export default (browser) => {
         apply: async function(url) {
             const page = await loadPage(browser, url);
             const text = await lookupText(page);
-            const date = lookupDate(text)
+            let date;
+            try {
+                date = lookupDate(text);
+            } catch(err) {
+                console.log("Could not parse a valid date => \n", err);
+            }
             return date;  
        } 
     }
